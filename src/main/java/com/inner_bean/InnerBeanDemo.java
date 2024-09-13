0b756e2aa8ff59c0858ddf61b78a7f370f05e8d3
surajsuresh9 <surajsuresh9@gmail.com>
2024-09-13 15:37:52 +0530

    add bean scope

--- src/main/java/com/inner_bean/InnerBeanDemo.java
+++ src/main/java/com/inner_bean/InnerBeanDemo.java
@@ -7,7 +7,13 @@ public class InnerBeanDemo {
     public static void main(String[] args) {
         AbstractApplicationContext abstractApplicationContext = new ClassPathXmlApplicationContext("inner_beans_config.xml");
         Employee e1 = (Employee) abstractApplicationContext.getBean("e1");
+        Employee e2 = (Employee) abstractApplicationContext.getBean("e1");
         System.out.println(e1.getId());
         System.out.println(e1.getAddress());
+        System.out.println("e1 hashCode: " + e1.hashCode()); // default scope is singleton
+        System.out.println("e2 hashCode: " + e2.hashCode());
+
+//        e1 hashCode: 11249189
+//        e2 hashCode: 159290353
     }
 }
